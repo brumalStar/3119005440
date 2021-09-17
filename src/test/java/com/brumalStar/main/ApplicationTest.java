d6081f53ea439a691c9ae851134897686727f115
3119005440 <zyw120817@163.com>
2021-09-17 22:15:21 +0800

    单元测试完成

--- src/test/java/com/brumalStar/main/ApplicationTest.java
+++ src/test/java/com/brumalStar/main/ApplicationTest.java
@@ -1,4 +1,84 @@
 package com.brumalStar.main;
 
+import com.brumalStar.utils.calcSimilarityUtil;
+import com.brumalStar.utils.convertUtils;
+import org.junit.Test;
+
 public class ApplicationTest {
+
+    @Test
+    public void origAndOthersTest1() {
+        // 测试文本1
+        System.out.println("test JProfiler start");
+        String[] str = new String[6];
+        str[0] = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        str[1] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_add.txt");
+        str[2] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_del.txt");
+        str[3] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_dis_1.txt");
+        str[4] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_dis_10.txt");
+        str[5] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_dis_15.txt");
+        String resultTxt1 = "G:\\resultTxt\\resultAll1.txt";
+        for (int i = 1; i < str.length; i++) {
+            double result = calcSimilarityUtil.calcSimilarity(calcSimilarityUtil.calcSimHash(str[0]), calcSimilarityUtil.calcSimHash(str[i]));
+            convertUtils.convertResult2Txt(result, resultTxt1);
+        }
+        System.out.println("test JProfiler end");
+    }
+
+    @Test
+    public void origAndAddTest(){
+        String[] str = new String[2];
+        str[0] = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        str[1] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_add.txt");
+        int distance = calcSimilarityUtil.calcHammingDistance(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        double similarity = calcSimilarityUtil.calcSimilarity(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        System.out.println("海明距离：" + distance);
+        System.out.println("原文本和抄袭文本的相似度：" + similarity);
+    }
+
+    @Test
+    public void origAndDelTest(){
+        String[] str = new String[2];
+        str[0] = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        str[1] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_del.txt");
+        int distance = calcSimilarityUtil.calcHammingDistance(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        double similarity = calcSimilarityUtil.calcSimilarity(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        System.out.println("海明距离：" + distance);
+        System.out.println("原文本和抄袭文本的相似度：" + similarity);
+    }
+
+    @Test
+    public void origAndDis_1Test(){
+        String[] str = new String[2];
+        str[0] = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        str[1] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_dis_1.txt");
+        int distance = calcSimilarityUtil.calcHammingDistance(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        double similarity = calcSimilarityUtil.calcSimilarity(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        System.out.println("海明距离：" + distance);
+        System.out.println("原文本和抄袭文本的相似度：" + similarity);
+    }
+
+    @Test
+    public void origAndDis_10Test(){
+        String[] str = new String[2];
+        str[0] = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        str[1] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_dis_10.txt");
+        int distance = calcSimilarityUtil.calcHammingDistance(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        double similarity = calcSimilarityUtil.calcSimilarity(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        System.out.println("海明距离：" + distance);
+        System.out.println("原文本和抄袭文本的相似度：" + similarity);
+    }
+
+    @Test
+    public void origAndDis_15Test(){
+        String[] str = new String[2];
+        str[0] = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        str[1] = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_dis_15.txt");
+        int distance = calcSimilarityUtil.calcHammingDistance(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        double similarity = calcSimilarityUtil.calcSimilarity(calcSimilarityUtil.calcSimHash(str[0]),calcSimilarityUtil.calcSimHash(str[1]));
+        System.out.println("海明距离：" + distance);
+        System.out.println("原文本和抄袭文本的相似度：" + similarity);
+    }
+
+
 }
--- src/test/java/com/brumalStar/utils/calcSimilarityUtilTest.java
+++ src/test/java/com/brumalStar/utils/calcSimilarityUtilTest.java
@@ -1,4 +1,50 @@
 package com.brumalStar.utils;
+import org.junit.Test;
 
 public class calcSimilarityUtilTest {
+
+    @Test
+    public void calcHashTest(){
+        // 测试用MD5获取128位的hash值
+        String[] strings = {"程序员", "是", "从事","程序", "开发", "维护","专业","人员"};
+        for(String item : strings){
+            String testHash = calcSimilarityUtil.calcHashByMd5(item);
+            System.out.println(testHash.length());
+            System.out.println(testHash);
+        }
+    }
+
+    @Test
+    public void calcSimHashTest(){
+        // 测试经过加权降维得到的SimHash
+        String str1 = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        String str2 = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_add.txt");
+        System.out.println(calcSimilarityUtil.calcSimHash(str1));
+        System.out.println(calcSimilarityUtil.calcSimHash(str2));
+
+    }
+
+    @Test
+    public void calcHammingDistanceTest(){
+        String str1 = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        String str2 = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_add.txt");
+        int hammingDistance = calcSimilarityUtil.calcHammingDistance(calcSimilarityUtil.calcSimHash(str1),
+                calcSimilarityUtil.calcSimHash(str2));
+        System.out.println("海明距离为：" + hammingDistance);
+        System.out.println("文本相似度为：" + 0.01 * (100 - hammingDistance * 100 / 128));
+
+    }
+
+    @Test
+    public void calcSimilarityTest(){
+        String str1 = convertUtils.convertTxt2String("G:\\testText\\orig.txt");
+        String str2 = convertUtils.convertTxt2String("G:\\testText\\orig_0.8_add.txt");
+        int hammingDistance = calcSimilarityUtil.calcHammingDistance(calcSimilarityUtil.calcSimHash(str1),
+                calcSimilarityUtil.calcSimHash(str2));
+        double similarity = calcSimilarityUtil.calcSimilarity(calcSimilarityUtil.calcSimHash(str1),
+                calcSimilarityUtil.calcSimHash(str2));
+
+        System.out.println("原文本和抄袭文本的海明距离为：" + hammingDistance);
+        System.out.println("原文本和抄袭文本的文本相似度为：" + similarity );
+    }
 }
